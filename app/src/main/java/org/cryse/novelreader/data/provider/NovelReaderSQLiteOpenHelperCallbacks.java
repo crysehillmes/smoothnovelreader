243ca48146cddda2a621ed633ac2a06e10490cf0
Cryse Hillmes <tyk5555@hotmail.com>
2015-08-05 18:40:24 +0800

    添加数据库更新代码。

--- app/src/main/java/org/cryse/novelreader/data/provider/NovelReaderSQLiteOpenHelperCallbacks.java
+++ app/src/main/java/org/cryse/novelreader/data/provider/NovelReaderSQLiteOpenHelperCallbacks.java
@@ -33,5 +33,27 @@ public class NovelReaderSQLiteOpenHelperCallbacks {
     public void onUpgrade(final Context context, final SQLiteDatabase db, final int oldVersion, final int newVersion) {
         if (BuildConfig.DEBUG) Log.d(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion);
         // Insert your upgrading code here.
+        if(oldVersion < 2) {
+            db.execSQL("DROP TABLE IF EXISTS " + "NOVEL_BOOK_MARK_MODEL;");
+            db.execSQL("DROP TABLE IF EXISTS " + "NOVEL_CHAPTER_CONTENT_MODEL;");
+            db.execSQL("DROP TABLE IF EXISTS " + "NOVEL_CHAPTER_MODEL;");
+            db.execSQL("DROP TABLE IF EXISTS " + "NOVEL_MODEL;");
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_TABLE_BOOKMARK);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_BOOKMARK_NOVEL_ID);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_BOOKMARK_CHAPTER_ID);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_TABLE_CHAPTER);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_CHAPTER_NOVEL_ID);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_CHAPTER_CHAPTER_ID);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_TABLE_CHAPTER_CONTENT);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_CHAPTER_CONTENT_NOVEL_ID);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_CHAPTER_CONTENT_CHAPTER_ID);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_CHAPTER_CONTENT_SOURCE);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_TABLE_NOVEL);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_NOVEL_NOVEL_ID);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_NOVEL_TITLE);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_NOVEL_AUTHOR);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_NOVEL_TYPE);
+            db.execSQL(NovelReaderSQLiteOpenHelper.SQL_CREATE_INDEX_NOVEL_SOURCE);
+        }
     }
 }
